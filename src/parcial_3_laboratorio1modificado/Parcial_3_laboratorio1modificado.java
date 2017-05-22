ac1bfb5e64a351a11e1b664f92f1c082e40904a7
sebas02 <spalacio00@misena.edu.co>
2017-05-22 11:11:54 -0500

    modificacion proyecto3

---
+++ src/parcial_3_laboratorio1modificado/Parcial_3_laboratorio1modificado.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial_3_laboratorio1modificado;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author William
+ */
+public class Parcial_3_laboratorio1modificado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero"));
+        int b = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero otro numero"));
+
+        if (a == b) {
+            System.err.println("El número " + b + " es igual que " + a);
+        } else {
+            System.err.println("El número " + a + " es diferente que " + b);
+        }
+        if (a > b) {
+            System.err.println("El número " + b + " es igual que " + a);
+        } else {
+            System.err.println("El número " + a + " es diferente que " + b);
+        }
+    }
+    
+}
